146bcf9e1f013a25a9757dc4e11190b73f71d29f
laras126 <laras126@gmail.com>
2014-06-15 08:53:10 -0400

    semanticize classes and main markup

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,12 @@
 
+        <footer class="footer">
+            <div class="contain">
+                <h3>footer</h3>
+                <p>Some copyright information for yo mama!</p>    
+            </div>
+        </footer>
+
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="build/js/production.min.js"></script>
--- index.php
+++ index.php
@@ -1,74 +1,13 @@
 <?php include('include/header.php'); ?>
 
-        <header class="wrap clearfix animated bounceInDown">
-            <h1 class="title">Lara's Website Starter Kit</h1>
-            <h3>whats dis?</h3>
-            <p>A boilerplate for custom websites, created by Lara! It's annoying to have to keep putting together tools you use for every website. As I'm sure many designers do, I have made myself a starter kit that includes everything I use to start a site. Maybe other people would be interested, so here it is! Of course, this version will be better documented as time goes on.</p>
-        </header>
-
-        <div class="main wrap clearfix animated bounceInUp">
-            <h5>In sum:</h5>
-            <p>This is a hybrid of <a href="http://www.initializr.com/" target="blank">Intializr</a> and the style conventions used in the WP starter theme <a href="http://themble.com/bones/" taget="blank">Bones</a>. <strong>SASS only</strong>.</p>
-        
-            <h5>Tools included:</h5>
-            <ul>
-                <li>Icon font: <a href="http://fortawesome.github.com/Font-Awesome/" target="blank">Font Awesome</a></li>
-                <li><a href="http://daneden.me/animate/">Animate.css</a></li>
-                <li><a href="http://24ways.org/2013/grunt-is-not-weird-and-hard/">Grunt</a></li>
-            </ul>
-    
-            <h5><span class="h3"><i class="fa fa-github"></i></span>&nbsp;It's on Github!</h5>
-            <a class="blue-button" href="https://github.com/laras126/starter-kit/zipball/master">Download here</a>
-            <p>Hey, you could <a href="https://github.com/laras126/starter-kit" target="blank">fork</a> and made it your own!</p>
-
-            <div class="clearfix">
-                <div class="sixcol">
-                    <header>
-                        <h2>About Headings</h2>
-                        <p>One thing Bones does that I like a lot is adding <code>h1-h5</code> <em>classses</em> so you can style arbitrary text. All of these are <code>p</code> tags with those classes:</p>
-                    </header>
-                    <section>
-                        <p class="h1">class .h1</p>
-                        <p class="h2">class .h2</p>
-                        <p class="h3">class .h3</p>
-                        <p class="h4">class .h4</p>
-                        <p class="h5">class .h5</p>
-                    </section>
-                    <footer>
-                        <h4>article footer h4</h4>
-                        <p>You could argue that this violates the "don't use <code>.blue</code> for the style blue", but I think this saves a lot of trouble styling individual elements. but what do i know!</p>
-                    </footer>
+        <main class="main" role="main">
+            <div class="content contain">
+                
+                <div class="gallery">
+                    
                 </div>
 
-                 <div class="sixcol last">
-                    <header>
-                        <h2>article header h2</h2>
-                        <p>High life squid consequat laborum, keytar dolore retro. Pork belly etsy gluten-free american apparel. Veniam velit ad, mixtape fingerstache minim dreamcatcher chillwave craft beer etsy biodiesel enim.</p>
-                    </header>
-                    <section>
-                        <h3>Some Style Deets (h3)</h3>
-                        <p>&darr; an <strong>unordeered</strong> list &darr;</p>
-                        <ul>
-                            <li>Strong: <strong>Culpa vero</strong>.</li>
-                            <li>Emphasis: <em>Salvia mollit brunch</em>.</li>
-                            <li>Ampersand: <span class="amp">&amp;</span></li>
-                        </ul>
-                        <p>Whatever bicycle rights blog ethnic, tattooed pinterest biodiesel delectus assumenda sunt vinyl. Mumblecore wes anderson sapiente, cardigan viral polaroid echo park tattooed vero. Sapiente sunt wes anderson, tofu umami art party nihil anim brunch eiusmod aute. Pour-over velit leggings bespoke.</p>
-                        <p>&darr; an <strong>ordeered</strong> list &darr;</p>
-                        <ol>
-                            <li>Strong: <strong>Culpa vero</strong>.</li>
-                            <li>Emphasis: <em>Salvia mollit brunch</em>.</li>
-                            <li>Ampersand: <span class="amp">&amp;</span></li>
-                        </ol>
-                    </section>
-                </div>
-            </div>
-
-        </div> <!-- .main -->
-
-        <footer class="wrap clearfix">
-            <h3>footer</h3>
-            <p>Some copyright information for yo mama!</p>
-        </footer>
+            </div> <!-- /.contain -->
+        </main> <!-- /.main -->
 
 <?php include('include/footer.php'); ?>
