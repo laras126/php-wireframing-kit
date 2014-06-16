9a91972a89f021e909b27246f98dc1d00cd46771
laras126 <laras126@gmail.com>
2014-06-16 07:17:04 -0400

    home page content, add an about page

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php 
+$contain = true;
+include('include/header.php'); ?>
+
+    <?php printCallout("Our promise to you.", "Choosing the sky will be the best decision you have ever made."); ?>    
+    <div class="col-8">
+        <p>We are a startup based in New York City, the greatest place in the world. People think New Yorkers are mean, but they aren't.</p>
+
+        <p>Anyway, our company will give you the best service with sun, a moon, and sometimes rain to cool things down. Our competitor, the Planets, only provide you with views of the sky and pretty much nothing else!</p>
+    </div>
+    <div class="col-4">
+        <?php printFigure("milkyway.jpeg", "Just look at how beautiful our friend the Milky Way is."); ?>
+    </div>
+
+<?php include('include/footer.php'); ?>
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<?php include('include/header.php'); ?>
-
-        <main class="main" role="main">
-            <div class="content contain">
+<?php 
+$contain = true;
+include('include/header.php'); ?>
                 
-                <div class="gallery">
-                    
-                </div>
+    <?php printCallout('The sky is the best.', 'And we are thinking about making an app for it.'); ?> 
 
-            </div> <!-- /.contain -->
-        </main> <!-- /.main -->
+	<div class="col-12">
+		<?php printHomeIcon('blackhole.jpeg', 'Never ending.', "Black holes are pretty scary, right? Well, with the sky you don't need to worry about them that much. This doesn't really make sense, but don't worry about it! Case in point."); ?>
+		<?php printHomeIcon('cloud.jpeg', 'Gorgeous clouds.', "Don't you wish you could jump on the clouds? We do too. Someday you might be able to, thanks to us. They are so soft and look like they would be great for sleeping."); ?>
+		<?php printHomeIcon('plane.jpeg', 'It transports you.', 'The sky provides a great way to get around. The primary way is with planes, but you can use a hang glider or parachute too!'); ?>
+	</div>
+
 
 <?php include('include/footer.php'); ?>
