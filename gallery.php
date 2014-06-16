752046e6e0ea39b202ae67ca278d7b85514a2a7f
laras126 <laras126@gmail.com>
2014-06-16 07:17:29 -0400

    add normalize and gallery page

---
+++ gallery.php
@@ -0,0 +1,10 @@
+<?php 
+
+$contain = true;
+
+include('include/header.php'); ?>
+        
+    <?php printCallout('Here are pictures of the sky.', 'Our app will show you them.'); ?> 
+    <?php printGallery('sky', 'Sky Gallery'); ?>
+
+<?php include('include/footer.php'); ?>
