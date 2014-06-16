096df196a05f0e344b4caee9c8f5e60398b00719
laras126 <laras126@gmail.com>
2014-06-16 17:46:47 -0400

    remove main markup from your index

--- your_index.php
+++ your_index.php
@@ -1,10 +1,6 @@
-<?php include('include/header.php'); ?>
+<?php 
+$contain = true;
+include('include/header.php'); ?>
 
-        <main class="main" role="main">
-            <div class="content contain">
-                
-
-            </div> <!-- /.contain -->
-        </main> <!-- /.main -->
 
 <?php include('include/footer.php'); ?>
