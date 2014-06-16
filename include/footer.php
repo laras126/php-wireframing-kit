a34e3a392f41dc4df3e64e5c7815adf21e031071
laras126 <laras126@gmail.com>
2014-06-16 18:26:46 -0400

    fix js path

--- include/footer.php
+++ include/footer.php
@@ -15,7 +15,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="build/js/production.min.js"></script>
+        <script src="assets/js/build/production.min.js"></script>
 
         <script>
             // Analytics here, sweetie
