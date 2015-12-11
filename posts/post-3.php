ded9ff2b477031d279d33443cbf22f37bb0bfd39
Chris Coyier <chriscoyier@gmail.com>
2015-12-11 16:12:18 -0600

    adding an extra blog post

---
+++ posts/post-3.php
@@ -0,0 +1,3 @@
+<h3>Chris' New Blog Post</h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus qui, mollitia sit incidunt cum facilis tenetur. In earum nobis, reiciendis, itaque tempore voluptate beatae sed hic! Totam, commodi eveniet natus.</p>
